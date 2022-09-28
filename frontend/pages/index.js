f456232673c6c7477f14fb2733d48f48626f5801
bagasalim <bagasalim10@gmail.com>
2022-09-28 16:01:38 +0700

    redploy lagi

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -39,7 +39,8 @@ function Input({ onSuccess }) {
       text: formData.get("data"),
     };
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/send`, {
+      console.log(process.env.NEXT_PUBLIC_BACKEND);
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND}/send`, {
         method: "POST",
         body: JSON.stringify(body),
       });
